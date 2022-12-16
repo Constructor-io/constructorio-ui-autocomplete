1d92fe23bfc3f50430976d9c7f6a57c222c2b629
Jesse DeOms <jesse.deoms@gmail.com>
2022-12-16 13:57:26 +0100

    start user on docs
    - and change order of viewMode tabs
    - and simplify docs page layout

---
+++ .storybook/preview.jsx
@@ -0,0 +1,28 @@
+import { Title, Description, Stories } from '@storybook/addon-docs';
+
+export const parameters = {
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/
+    }
+  },
+  options: {
+    storySort: (a, b) =>
+      a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true })
+  },
+  layout: 'fullscreen',
+  viewMode: 'docs',
+  previewTabs: {
+    'storybook/docs/panel': { index: -1 }
+  },
+  docs: {
+    page: () => (
+      <>
+        <Title />
+        <Description />
+        <Stories />
+      </>
+    )
+  }
+};
