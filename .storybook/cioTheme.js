710a7497ac0e79772665d4e92961516d2f4736d0
Jesse DeOms <jesse.deoms@gmail.com>
2023-02-01 16:37:45 -0500

    replace sb icons with cio icons

---
+++ .storybook/cioTheme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  brandTitle: 'Constructor.io',
+  brandUrl: 'https://github.com/Constructor-io/constructorio-ui-autocomplete',
+  brandImage: 'https://docs.constructor.io/img/logo-light.svg',
+  brandTarget: '_blank',
+});
