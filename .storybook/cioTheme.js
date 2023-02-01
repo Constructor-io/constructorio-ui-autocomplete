781a9bc55362c2b143324c6f6204a303f230d6b7
Jesse DeOms <jesse.deoms@gmail.com>
2023-02-01 18:15:55 -0500

    Simplify Storybook Configs

--- .storybook/cioTheme.js
+++
@@ -1,8 +0,0 @@
-import { create } from '@storybook/theming/create';
-
-export default create({
-  brandTitle: 'Constructor.io',
-  brandUrl: 'https://github.com/Constructor-io/constructorio-ui-autocomplete',
-  brandImage: 'https://docs.constructor.io/img/logo-light.svg',
-  brandTarget: '_blank',
-});
