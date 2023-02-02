a0b8c2605ec0f1d95334df65ca85feae357da0a9
Enes Kutay SEZEN <eneskutaysezen@gmail.com>
2023-02-02 13:25:55 -0500

    Clean up

--- docs/sb-addons/.storybook/manager-bundle.mjs
+++
@@ -1,4 +0,0 @@
-try{
-var s=__STORYBOOKADDONS__,{addons:t,types:n,mockChannel:c}=__STORYBOOKADDONS__;var d=__STORYBOOKTHEMING__,{CacheProvider:_,ClassNames:p,Global:h,ThemeProvider:O,background:g,color:u,convert:T,create:r,createCache:b,createGlobal:f,createReset:C,css:S,darken:x,ensure:k,ignoreSsrWarning:y,isPropValid:N,jsx:R,keyframes:v,lighten:B,styled:D,themes:G,typography:K,useTheme:Y,withTheme:I}=__STORYBOOKTHEMING__;var a=r({brandTitle:"Constructor.io",brandUrl:"https://github.com/Constructor-io/constructorio-ui-autocomplete",brandImage:"https://docs.constructor.io/img/logo-light.svg",brandTarget:"_blank"});t.setConfig({theme:a});
-}catch(e){ console.error("[Storybook] One of your manager-entries failed: " + import.meta.url, e); }
-//# sourceMappingURL=manager-bundle.mjs.map
