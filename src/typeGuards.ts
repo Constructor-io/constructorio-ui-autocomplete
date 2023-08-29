82b2e55bd50fb413e5c7a45c61b7cf51910effd6
Enes Kutay SEZEN <eneskutaysezen@gmail.com>
2023-08-29 18:04:06 -0400

    Update types

--- src/typeGuards.ts
+++ src/typeGuards.ts
@@ -1,7 +1,18 @@
-import { CustomSection, InGroupSuggestion, Item, Product, UserDefinedSection } from './types';
+import {
+  CustomSection,
+  InGroupSuggestion,
+  Item,
+  Product,
+  UserDefinedSection,
+  SearchSuggestion,
+} from './types';
 
 export function isProduct(item: Item): item is Product {
-  return (item as Product).data?.image_url !== undefined;
+  return (item as Product).section === 'Products';
+}
+
+export function isSearchSuggestion(item: Item): item is SearchSuggestion {
+  return (item as SearchSuggestion).section === 'Search Suggestions';
 }
 
 export function isInGroupSuggestion(item: Item): item is InGroupSuggestion {
