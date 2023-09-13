0ae6ac5583cce53dda51a662accda09fa4d31c2f
Islam Moustafa <58053149+mocca102@users.noreply.github.com>
2023-09-13 18:56:44 +0300

    Override c parameters for the cioClient (#76)
    * Override c parameters for the cioClient
    
    * Rename c parameter

--- src/utils.ts
+++ src/utils.ts
@@ -101,6 +101,7 @@ export const getCioClient = (apiKey?: string) => {
     return new ConstructorIOClient({
       apiKey,
       sendTrackingEvents: true,
+      version: 'cio-ui-autocomplete',
     });
   }
 
