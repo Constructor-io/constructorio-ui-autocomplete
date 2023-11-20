e538c6a0a76072027c84e77aec01f3c00544c349
Guilherme Frota <guiquintelas96@gmail.com>
2023-11-20 19:44:04 -0300

    lots of stuff

--- src/generateVersion.js
+++
@@ -1,5 +0,0 @@
-const { writeFileSync } = require('fs');
-const { version } = require('../package.json');
-
-const content = `export default '${version}';\n`;
-writeFileSync('./src/version.ts', content);
