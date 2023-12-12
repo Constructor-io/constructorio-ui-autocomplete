065c940e6a89ff440ada0b8953e8e89a774b2053
Enes Kutay SEZEN <eneskutaysezen@gmail.com>
2023-12-12 16:02:42 -0500

    Update camelToStartCase function

--- src/utils.ts
+++ src/utils.ts
@@ -50,7 +50,8 @@ export const camelToStartCase: CamelToStartCase = (camelCaseString) =>
     // insert a space before all caps
     .replace(/([A-Z])/g, ' $1')
     // uppercase the first character
-    .replace(/^./, (str) => str.toUpperCase());
+    .replace(/^./, (str) => str.toUpperCase())
+    .trim();
 
 export function isTrackingRequestSent(trackingRequestUrl) {
   // eslint-disable-next-line
