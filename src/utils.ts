b38622018cf8c1c6a6b8d3e56f18a82e468789b2
Islam Moustafa <58053149+mocca102@users.noreply.github.com>
2023-12-29 20:15:22 +0200

    Update storybook version to v7.6.6 (#117)
    * Update storybook version to v7.6.6
    
    * Revert type changes
    
    * Remove as any

--- src/utils.ts
+++ src/utils.ts
@@ -5,7 +5,7 @@ import {
   ConstructorClientOptions,
 } from '@constructor-io/constructorio-client-javascript/lib/types';
 import { isCustomSection } from './typeGuards';
-import { OnSubmit, Item, Section, UserDefinedSection, SectionsData, Translations } from './types';
+import { Item, Section, UserDefinedSection, SectionsData, Translations } from './types';
 import version from './version';
 
 export type GetItemPosition = (args: { item: Item; items: Item[] }) => {
@@ -99,7 +99,7 @@ export const functionStrings = {
   onSubmit: `(submitEvent) => console.dir(submitEvent)`,
 };
 
-export const stringifyWithDefaults = (obj: { apiKey: string; onSubmit: OnSubmit }) => {
+export const stringifyWithDefaults = (obj) => {
   // Stringify non-function values normally. Add a template block for functions to be replaced later
   let res = JSON.stringify(
     obj,
