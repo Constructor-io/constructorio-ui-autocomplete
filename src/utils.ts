1848d184b3f3240fb50bd94a6e9060649ccf8d64
Enes Kutay SEZEN <eneskutaysezen@gmail.com>
2024-01-03 09:56:44 -0500

    Typo

--- src/utils.ts
+++ src/utils.ts
@@ -57,7 +57,7 @@ export const toKebabCase = (str: string): string =>
   str
     .replace(/([A-Z])([A-Z][a-z])/g, '$1-$2')
     .replace(/([a-z])([A-Z])/g, '$1-$2')
-    .replace(/ [\s_]+/g, '-')
+    .replace(/[\s_]+/g, '-')
     .toLowerCase();
 
 export function isTrackingRequestSent(trackingRequestUrl: string) {
