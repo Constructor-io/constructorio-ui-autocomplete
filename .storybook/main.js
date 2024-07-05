dc3e2d93a505ae316dd8f0c74298a33d46b56c62
Stanley Peng <speng1011@gmail.com>
2024-07-05 00:56:47 -0700

    storybook docs

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,7 +4,8 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    '@storybook/addon-a11y'
+    '@storybook/addon-a11y',
+    '@storybook/addon-docs',
   ],
   framework: {
     name: '@storybook/react-webpack5',
