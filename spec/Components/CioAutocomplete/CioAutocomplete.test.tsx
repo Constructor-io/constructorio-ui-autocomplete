efdf23a7968a9300a107e4664eedb2d9b7ad7672
Islam Moustafa <58053149+mocca102@users.noreply.github.com>
2024-07-22 11:03:19 +0300

    Ci 3197 os UI autocomplete component index stories tsx add client side (#148)
    * Setup jest testing environment
    
    * CioAutocomplete client tests
    
    * CioAutocomplete server tests
    
    * fix typo
    
    * Remove only
    
    * update tests
    
    * move tests into their test suite
    
    * Updates

--- spec/Components/CioAutocomplete/CioAutocomplete.test.tsx
+++ spec/Components/CioAutocomplete/CioAutocomplete.test.tsx
@@ -1,15 +1,22 @@
 /* eslint-disable no-console */
+import '@testing-library/jest-dom';
 import React from 'react';
 import { render } from '@testing-library/react';
 import { CioAutocomplete } from '../../../src';
+import { mockCioClientJS } from '../../test-utils';
+import { apiKey as DEMO_API_KEY, onSubmitDefault as onSubmit } from '../../../src/constants';
 
-describe('CioAutocomplete React Client-Side Rendering', () => {
+describe('CioAutocomplete Client-Side Rendering', () => {
   beforeEach(() => {
     jest.spyOn(console, 'error').mockImplementation(() => {});
   });
 
   afterEach(() => {
     jest.restoreAllMocks();
+  });
+
+  afterAll(() => {
+    jest.resetAllMocks();
   });
 
   it("Logs an error if apiKey or cioJsClient isn't provided", () => {
@@ -19,5 +26,104 @@ describe('CioAutocomplete React Client-Side Rendering', () => {
     expect(console.error).toHaveBeenCalledWith(
       expect.stringContaining('Either apiKey or cioJsClient is required')
     );
+  });
+
+  it("Doesn't throw error if we provide an API key", () => {
+    expect(() => {
+      render(<CioAutocomplete apiKey={DEMO_API_KEY} onSubmit={() => {}} />);
+    }).not.toThrow();
+
+    expect(console.error).not.toHaveBeenCalled();
+  });
+
+  it("Doesn't throw error if we provide a ConstructorIO Client", () => {
+    expect(() => {
+      render(<CioAutocomplete cioJsClient={mockCioClientJS()} onSubmit={() => {}} />);
+    }).not.toThrow();
+
+    expect(console.error).not.toHaveBeenCalled();
+  });
+
+  it("Doesn't throw error if we provide a CIOClientOptions", () => {
+    const cioJsClientOptions = { apiKey: DEMO_API_KEY, serviceUrl: 'https://ac.cnstrc.com' };
+
+    expect(() => {
+      render(
+        <CioAutocomplete
+          apiKey={DEMO_API_KEY}
+          cioJsClientOptions={cioJsClientOptions}
+          onSubmit={() => {}}
+        />
+      );
+    }).not.toThrow();
+
+    expect(console.error).not.toHaveBeenCalled();
+  });
+
+  it('Render custom placeholder when passed as a prop', () => {
+    const { getByPlaceholderText } = render(
+      <CioAutocomplete
+        apiKey={DEMO_API_KEY}
+        placeholder='Custom placeholder text'
+        onSubmit={() => {}}
+      />
+    );
+
+    expect(getByPlaceholderText('Custom placeholder text')).toBeInTheDocument();
+  });
+
+  it('Accepts custom styles when passed as a prop', () => {
+    const { container } = render(
+      <CioAutocomplete
+        apiKey={DEMO_API_KEY}
+        autocompleteClassName='cio-autocomplete custom-autocomplete-styles'
+        onSubmit={() => {}}
+      />
+    );
+
+    expect(container.querySelector('.custom-autocomplete-styles')).toBeInTheDocument();
+  });
+
+  it("Fully featured example doesn't throw an error", () => {
+    enum Type {
+      recommendations = 'recommendations',
+    }
+
+    const props = {
+      cioJsClient: mockCioClientJS(),
+      onSubmit,
+      autocompleteClassName: '',
+      placeholder: 'What can we help you find?',
+      advancedParameters: {
+        displaySearchSuggestionImages: true,
+        displaySearchSuggestionResultCounts: true,
+        numTermsWithGroupSuggestions: 6,
+      },
+      sections: [
+        {
+          indexSectionName: 'Search Suggestions',
+          numResults: 8,
+          displaySearchTermHighlights: true,
+        },
+        {
+          indexSectionName: 'Products',
+          numResults: 6,
+          displaySearchTermHighlights: true,
+        },
+      ],
+      zeroStateSections: [
+        {
+          podId: 'bestsellers',
+          type: Type.recommendations,
+          numResults: 6,
+        },
+      ],
+    };
+
+    expect(() => {
+      render(<CioAutocomplete {...props} />);
+    }).not.toThrow();
+
+    expect(console.error).not.toHaveBeenCalled();
   });
 });
