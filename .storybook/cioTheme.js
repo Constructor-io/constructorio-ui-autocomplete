ea8fc648501c36ed0a9b5d666677f0b7e5483932
Steve Blaurock <sblaurock@gmail.com>
2024-11-06 20:11:59 -0700

    Revert "Use preferred color scheme"
    This reverts commit ecefd42977426d2ff66218b8fcd923a48cad19a8.

--- .storybook/cioTheme.js
+++ .storybook/cioTheme.js
@@ -3,6 +3,6 @@ import { create } from '@storybook/theming/create';
 export default create({
   brandTitle: 'Constructor',
   brandUrl: 'https://github.com/Constructor-io/constructorio-ui-autocomplete',
-  brandImage:  getPreferredColorScheme() === 'light' ? 'https://constructor.com/hubfs/Website%20-%202024/Logos/Logo-black.svg' : 'https://constructor.com/hubfs/Website%20-%202024/Logos/Logo-white.svg',
+  brandImage: 'https://constructor.com/hubfs/Website%20-%202024/Logos/Logo-white.svg',
   brandTarget: '_blank',
 });
