535dc91b490ab0c0051450d804292f2c647d22d1
mocca102 <islaam.agamy@gmail.com>
2025-03-17 12:23:20 +0200

    Address comments

--- src/hooks/useCustomBlur.ts
+++ src/hooks/useCustomBlur.ts
@@ -3,8 +3,6 @@ import { useCallback, useEffect } from 'react';
 const useCustomBlur = (isOpen: boolean, closeMenu: () => void) => {
   const handleDocumentClick = useCallback(
     (event: MouseEvent) => {
-      event.preventDefault();
-      event.stopPropagation();
       if (isOpen && !(event.target as HTMLElement)?.closest('.cio-autocomplete')) {
         closeMenu();
       }
@@ -13,9 +11,9 @@ const useCustomBlur = (isOpen: boolean, closeMenu: () => void) => {
   );
 
   useEffect(() => {
-    document.addEventListener('click', handleDocumentClick);
+    document.addEventListener('mousedown', handleDocumentClick);
     return () => {
-      document.removeEventListener('click', handleDocumentClick);
+      document.removeEventListener('mousedown', handleDocumentClick);
     };
   }, [handleDocumentClick]);
 };
